dc021c3880ccd70047a18d0563cf215d44f73bb2
Gareth Searle <SEA19004582@btc.ac.uk>
2022-11-17 15:27:10 +0000

    Completed Task 1 Solution 2, Worked on Tak 2 solution 2, Need to fix bubble sort within the Order Dictionary Method

--- Programming assessment/Program.cs
+++ Programming assessment/Program.cs
@@ -1,4 +1,6 @@
 ﻿//-----TASK 1-----
+//Solution1 = With Predefined Functions
+/*
 static void PrintList(List<int> list, string Title)//a mathod to print lists
 {
     Console.WriteLine($"{ Title }");
@@ -8,27 +10,6 @@ static void PrintList(List<int> list, string Title)//a mathod to print lists
     }
 }
 
-static List<int> BubbleSort(List<int> nums)//Bubble sort
-{
-    bool swapActive = false;
-    int temp = 0;
-    while (swapActive)
-    {
-        for (int i = 0; i <= nums.Count - 2; i++)
-        {
-            if (nums[i] > nums[i + 1])
-            {
-                temp = nums[i];
-                nums[i] = nums[i + 1];
-                nums[i + 1] = temp;
-                swapActive = true;
-            }
-        }
-    }
-
-    return nums;
-};
-
 List<int> Listofnums = new List<int>() //creates new list Containing 89, 42, 65 etc
 {
     89, 42, 65, 18, 73, 15, 6, 89, 71, 17, 11, 51, 55, 91
@@ -36,9 +17,9 @@ List<int> Listofnums = new List<int>() //creates new list Containing 89, 42, 65 
 
 PrintList(Listofnums, "Original List:");//calls function to display list
 
-List<int> sorted = BubbleSort(Listofnums);//calls function to sort list
+Listofnums.Sort();//calls function to sort list
 
-PrintList(sorted, "Sorted List:");//calls function to display the sorted list
+PrintList(Listofnums, "Sorted List:");//calls function to display the sorted list
 
 //count
 Console.WriteLine($"Count: { Listofnums.Count }");//displays length of the list
@@ -75,9 +56,144 @@ Console.WriteLine($"Mode: { CommonKey }");
 int len = Listofnums.Count();//works out the length of the list
 int median = len % 2;//works out which index will be the median value
 Console.WriteLine($"Median: { Listofnums[median] }");//displays the median of the list
+*/
 
+//Solution 2 = Without Predefined Functions
+static void PrintList(List<int> list, string Title)//a mathod to print lists
+{
+    Console.WriteLine($"{ Title }");
+    foreach (int i in list)
+    {
+        Console.WriteLine($"{ i }");
+    }
+}
+
+static List<int> BubbleSort(List<int> nums)//Bubble sort
+{
+    bool swapActive = true;
+    int temp = 0;
+    while (swapActive)
+    {
+        swapActive = false;
+        for (int i = 0; i <= nums.Count - 2; i++)
+        {
+            if (nums[i] > nums[i + 1])
+            {
+                temp = nums[i];
+                nums[i] = nums[i + 1];
+                nums[i + 1] = temp;
+                swapActive = true;
+            }
+        }
+    }
+
+    return nums;
+};
+
+static Dictionary<int, int> OrderDictionary(Dictionary<int, int> Dictionary)
+{
+    List<int> keys = new List<int>();
+    List<int> values = new List<int>();
+    foreach (int key in Dictionary.Keys)//gathers the dictionary keys into a list
+    {
+        keys.Add(key);
+    }
+    foreach (int Value in Dictionary.Values)//gathers the dictionary keys into a list
+    {
+        values.Add(Value);
+    }
+
+    bool swapActive = true;
+    int keytemp = 0;
+    int valuetemp = 0;
+
+    while (swapActive)
+    {
+        swapActive=false;
+        for (int i = 0; i <= Dictionary.Count - 2; i++)//organizes Both the list , using the value, making sure the key and value from the dictionary chare the same index
+        {
+            if (values[i] > values[i + 1])
+            {
+                keytemp = keys[i];
+                valuetemp = values[i];
+
+                keys[i] = keys[i + 1];
+                values[i] = values[i + 1];
+
+                keys[i + 1] = keytemp;
+                values[i + 1] = valuetemp;
+
+                swapActive = true;
+            }
+        }
+    }
+
+    Dictionary<int, int> SortedDict = new Dictionary<int, int>();//copies value using the keys from sorted list 
+    for (int j = 0; j <= values.Count - 1; j++)
+    {
+        int CorrectKey = keys[j];
+        int CorrectValue = values[j];
+        SortedDict.Add(CorrectKey, CorrectValue);
+    }
+
+    return SortedDict;
+
+}
+
+
+List<int> Listofnums = new List<int>() //creates new list Containing 89, 42, 65 etc
+{
+    89, 42, 65, 18, 73, 15, 6, 89, 71, 17, 11, 51, 55, 91
+};
+
+PrintList(Listofnums, "Original List:");//calls function to display list
+
+List<int> sorted = BubbleSort(Listofnums);//calls function to sort list
+
+PrintList(sorted, "Sorted List:");//calls function to display the sorted list
+
+//count
+Console.WriteLine($"Count: { Listofnums.Count }");//displays length of the list
+
+//mean
+int value = 0;
+int count = 0;
+for (int i = 0; i < Listofnums.Count; i++)//adds all the values together using the value variable
+{
+    value = Listofnums[i] + value;
+    count++;
+}
+
+Console.WriteLine($"Mean: { value / count }");//works out and displays the mean
+
+//mode
+Dictionary<int, int> Mode = new Dictionary<int, int>();//creates new dictionary
+for (int i = 0; i < Listofnums.Count; i++)//loops through all of the list
+{
+    if (!Mode.ContainsKey(Listofnums[i]))
+    { //works ou whether the number is in the dictionary already
+        Mode.Add(Listofnums[i], 0);//if it is NOT, then it will add as a new item into the dictionary
+    }
+    else
+    {
+        Mode[Listofnums[i]]++;//if it IS in the dictionary, then it will increment the value of the dictionary item
+    }
+}
+
+Dictionary<int,int> SortedDict = OrderDictionary(Mode);//orders the dictionary so the highest valued item is at the top. it then gets the key from the first item in the dictionary
+
+var CommonKey = SortedDict.First().Value;//gets first element from the dictionary
+
+Console.WriteLine($"Mode: { CommonKey }");
+
+//median
+int len = Listofnums.Count();//works out the length of the list
+int median = len % 2;//works out which index will be the median value
+Console.WriteLine($"Median: { Listofnums[median] }");//displays the median of the list
 
 //-----TASK 2-----
+
+// Solution 1 = With Predefined functions
 /*
 Queue<string> position = new Queue<string>();//creates queue
 position.Enqueue("One");//adds queue values
@@ -110,6 +226,51 @@ foreach (string i in position)
 Console.WriteLine("----------------------");
 
 Console.WriteLine(position.Count);
+*/
+
+//Solution 2 = Without Predefined functions
+/*
+static void PrintQueue(string title, List<string> Queue)
+{
+    Console.WriteLine(title);
+    foreach (string i in Queue)//prints queue
+    {
+        Console.WriteLine($"{ i }");
+    };
+
+    Console.WriteLine("----------------------");
+}
+
+static List<string> AddQueue(List<string> Queue, string Item)
+{
+    Queue.Insert(Queue.Count, Item);
+
+    return Queue;
+}
+
+static List<string> RemoveQueue(List<string> Queue)
+{
+    Queue.Remove(Queue[0]);
+
+
+    return Queue;
+}
+
+List<string> Queue = new List<string>()
+{
+    "One", "Two", "Three", "Four", "Five"
+};
+
+int QueueLength = Queue.Count;
+PrintQueue("Origional Queue:", Queue);
+
+Queue = AddQueue(Queue, "Six");//adds six to the end of the queue
+PrintQueue("Added 6:", Queue);
+
+Queue = RemoveQueue(Queue);//Removes one from the queue
+PrintQueue("Removed 1:", Queue);
+
+Console.WriteLine($"Count: { Queue.Count }");
 */
 
 //-----TASK 3-----
